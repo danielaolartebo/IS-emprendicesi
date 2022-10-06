4da50af1dccd9b1ae386c9347be89531c34142ec
Daniela Olarte <danielaolarte@Danis-MacBook-Air.local>
2022-10-06 08:49:37 -0500

    background set up

--- pages/index.js
+++ pages/index.js
@@ -2,27 +2,28 @@ import Welcome from '../components/Welcome'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
-
  
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Welcome title='EMPRENDICESI'></Welcome>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          EMPRENDICESI
-        </h1>
-        <br></br>
-        <br></br>
-        <div className={styles.grid}>
-          <Link href='../website/login'>
-            <a className={styles.card}>
-              <h2>Log in</h2>
-            </a>
-          </Link>
-        </div>
-      </main>
+  <div className={styles.contenedor}>
+      <img className={styles.back_image} src = "/images/BK-ROSETAS_FDCS.png"/>
+      <div className={styles.divxd}>
+        <Welcome title='EMPRENDICESI'></Welcome>
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            EMPRENDICESI
+          </h1>
+          <div className={styles.grid}>
+            <Link href='../website/login'>
+              <a className={styles.card}>
+                <h2>Log in</h2>
+              </a>
+            </Link>
+          </div>
+        </main>
+      </div>
+    
     </div>
   )
+  
 }
