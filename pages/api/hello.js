98a9a137070c1ca8e818c451eecf6f1a52dda27f
Brayan Ortiz <brayanortizbastidas82@gmail.com>
2022-11-27 13:42:26 -0500

    send form by email

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import sendEmail from "../../models/mails/mailer";
+export default async  function handler(req, res) {
 
-export default function handler(req, res) {
+  let emailTo = "algunemail@ejemplo.com"
+  let subject = "Mensaje desde servidor fantasma"
+  let content = '<h1> hello world </h1>' // ideally should be in html format
+  await sendEmail(emailTo, subject,content ).catch(console.error);
   res.status(200).json({ name: 'John Doe' })
-}
+}        
